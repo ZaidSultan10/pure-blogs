7d284d3f7dc18e19038e4e9f91df011688a1ebee
zaidsultan10 <zaidsultan09@gmail.com>
2023-06-15 23:25:36 +0400

    category wise featured blog skeleton set

---
+++ src/components/featuredBlogs/FeaturedBlogs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../styles/featuredBlogsStyles/_featuredBlogsStyles.scss'
+
+const FeaturedBlogs = () => {
+  return (
+    <section className='featuredBlogs__container'>FeaturedBlogs</section>
+  )
+}
+
+export default FeaturedBlogs
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../../components/banner/Banner'
+import FeaturedBlogs from '../../components/featuredBlogs/FeaturedBlogs'
 
 const Home = () => {
   return (
     <main className='home__container'>
         <Banner />
+        <FeaturedBlogs />
     </main>
   )
 }
