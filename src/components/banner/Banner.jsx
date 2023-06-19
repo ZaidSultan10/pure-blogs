541aac87ac29db305cf3635b5e0c3b28d59c04d2
zaidsultan10 <zaidsultan09@gmail.com>
2023-06-19 22:52:14 +0400

    banner slider make

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import '../../styles/bannerStyles/_bannerStyles.scss'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Banner = () => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyLoad: true,
+    autoplay: true,
+    autoplaySpeed: 2000,   
+    pauseOnHover: true
+  };
   return (
     <section className='banner__container'>
-        <div>banner</div>
+        <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
     </section>
   )
 }
