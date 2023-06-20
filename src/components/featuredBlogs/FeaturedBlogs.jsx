f655a8007a854d264d9acf1e119e1a58c003e580
zaidsultan10 <zaidsultan09@gmail.com>
2023-06-21 00:40:47 +0400

    banner slider make

--- src/components/featuredBlogs/FeaturedBlogs.jsx
+++ src/components/featuredBlogs/FeaturedBlogs.jsx
@@ -3,7 +3,64 @@ import '../../styles/featuredBlogsStyles/_featuredBlogsStyles.scss'
 
 const FeaturedBlogs = () => {
   return (
-    <section className='featuredBlogs__container'>FeaturedBlogs</section>
+    <section className='featuredBlogs__container'>
+      <div className='featuredBlogs__container__blogPost'>
+        <div className='featuredBlogs__container__blogPost__image'>
+          banner image
+        </div>
+        <div className='featuredBlogs__container__blogPost__title'>
+          <h4>banner title</h4>
+        </div>
+        <div className='featuredBlogs__container__blogPost__desc'>
+          <p>banner desc</p>
+        </div>
+        <div className='featuredBlogs__container__blogPost__link'>
+          <p>banner link</p>
+        </div>
+      </div>
+      <div className='featuredBlogs__container__blogPost'>
+        <div className='featuredBlogs__container__blogPost__image'>
+          banner image
+        </div>
+        <div className='featuredBlogs__container__blogPost__title'>
+          <h4>banner title</h4>
+        </div>
+        <div className='featuredBlogs__container__blogPost__desc'>
+          <p>banner desc</p>
+        </div>
+        <div className='featuredBlogs__container__blogPost__link'>
+          <p>banner link</p>
+        </div>
+      </div>
+      <div className='featuredBlogs__container__blogPost'>
+        <div className='featuredBlogs__container__blogPost__image'>
+          banner image
+        </div>
+        <div className='featuredBlogs__container__blogPost__title'>
+          <h4>banner title</h4>
+        </div>
+        <div className='featuredBlogs__container__blogPost__desc'>
+          <p>banner desc</p>
+        </div>
+        <div className='featuredBlogs__container__blogPost__link'>
+          <p>banner link</p>
+        </div>
+      </div>
+      <div className='featuredBlogs__container__blogPost'>
+        <div className='featuredBlogs__container__blogPost__image'>
+          banner image
+        </div>
+        <div className='featuredBlogs__container__blogPost__title'>
+          <h4>banner title</h4>
+        </div>
+        <div className='featuredBlogs__container__blogPost__desc'>
+          <p>banner desc</p>
+        </div>
+        <div className='featuredBlogs__container__blogPost__link'>
+          <p>banner link</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
